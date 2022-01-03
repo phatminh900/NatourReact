c240b0db61a514ec95754fa6eba92dbb4b4fa272
Phat TRan <phatminh900@gmail.com>
2022-01-03 11:43:19 +0700

    added alt for img

--- src/components/TourDetails/TourImgBox.js
+++ src/components/TourDetails/TourImgBox.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import styles from './TourImgBox.module.css'
-const TourImgBox = ({tourId}) => {
-    return (
-        <div className={`${styles['tour-img-box']} `}>
-          <img src={require(`../img/tours/timg${+tourId.slice(1)}.jpg`)} />
-          <img src={require(`../img/tours/timg${+tourId.slice(1)+1}.jpg`)} />
-          <img src={require(`../img/tours/timg${+tourId.slice(1)+2}.jpg`)} />
-      </div>
-    )
-}
+import React from "react";
+import styles from "./TourImgBox.module.css";
+const TourImgBox = ({ tourId }) => {
+  return (
+    <div className={`${styles["tour-img-box"]} `}>
+      <img
+        alt="Img box"
+        src={require(`../img/tours/timg${+tourId.slice(1)}.jpg`)}
+      />
+      <img
+        alt="Img box"
+        src={require(`../img/tours/timg${+tourId.slice(1) + 1}.jpg`)}
+      />
+      <img
+        alt="Img box"
+        src={require(`../img/tours/timg${+tourId.slice(1) + 2}.jpg`)}
+      />
+    </div>
+  );
+};
 
-export default TourImgBox
+export default TourImgBox;
